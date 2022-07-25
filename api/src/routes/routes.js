270070e55a7f099fd0a66755e78ff6bd7cc46229
Juan Prado <juan_prado_rojas@outlook.com>
2022-07-24 21:56:51 -0400

    Admin Route

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -5,6 +5,10 @@ const express = require("express");
 const animaldbregistration = require("./dbAnimalRegistration.js");
 const userdbregistration = require("./dbUserRegistration.js");
 const userlogin = require("./userLogin.js");
+const userlogout = require("./userLogout.js");
+const admin = require("./admin.js");
+const user = require("./user.js");
+const getAnimal = require("./getAnimal.js");
 
 const router = express.Router();
 
@@ -13,4 +17,7 @@ const router = express.Router();
 router.use("/", animaldbregistration);
 router.use("/", userdbregistration);
 router.use("/", userlogin);
+router.use("/", userlogout);
+router.use("/", admin);
+router.use("/", user);
 module.exports = router;
